d0589a41e45f33095fcc9ab4f39870bb19a4b323
brickbite <jeffreylee0116@gmail.com>
2017-09-05 16:07:39 -0700

    db config

---
+++ database/config.js
@@ -0,0 +1,3 @@
+const database_url = 'postgres://crpzncdo:fR_T57lmF2a1_oPDNd8flfi7kKUMTzpv@pellefant.db.elephantsql.com:5432/crpzncdo' //then change filename to config.js
+
+module.exports = database_url
